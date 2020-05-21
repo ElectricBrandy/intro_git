946e7f32c034d73eb6e9fa1b74d7bc016a2076a1
ElectricBrandy <yoshitaka.kishi@outlook.jp>
2020-05-21 17:37:58 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$scores =[50,40,70,90,70];
+
+//echo $scores[0];
+//echo"<br>";
+
+//foreach($scores as $scores){
+//   echo $scores;
+//    echo $"<br>";
+// }
+
+$sum = 0;
+
+foreach($scores as $scores){
+    $sum = $sum + $scores;
+    
+    echo "現在のスコア";
+    echo $scores0;
+    echo"<br>";
+
+    echo "現在の合計点";
+    echo $sum;
+    echo"<br>";
+}
+
+?>
