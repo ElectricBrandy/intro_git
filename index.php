f50195fc8c4a0a5ee9fc2af6b717f9ea40413c94
ElectricBrandy <yoshitaka.kishi@outlook.jp>
2020-05-29 13:49:19 +0900

    HTMLへの埋め込み記法
    学習のため

--- index.php
+++ index.php
@@ -1,25 +1,60 @@
 <?php
 
-$scores = [50,60,70,80,90];
-
-$tests = [
-    "japanese" => 50,
-    "math" => 60,
-    "Science" => 70,
-    "english" => 80,
-    "infomation" => 90,
+$data1 = [
+    "title" => "skillhacks",
+    "body" => "Web開発",
 ];
 
-echo $tests[math];
- echo "<br>";
-
-
-$user = [
-    "name" => "yoshitaka",
-    "age" => 22,
-    "company" => "kishi　corporation",
+$data2 = [
+    "title" => "bloghacks",
+    "body" => "ブログ開発",
 ];
 
-echo $user[name];
-    
+$data3 = [
+    "title" => "moviehacks",
+    "body" => "映像開発",
+];
+
+$data4 = [
+    "title" => "designhacks",
+    "body" => "デザイン開発",
+];
+
+$data5 = [
+    "title" => "frontskillhacks",
+    "body" => "フロントエンド開発",
+];
+
+$hacks = [$data1, $data2, $data3, $data4, $data5];
+
+// echo $hacks;
+// echo "<br>";
+// var_dump($hacks);
+// echo "<br>";
+
+// foreach($hacks as $hack){
+//     echo $hack["title"]."<br>";
+//     echo $hack["body"]."<br>";
+// }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="jn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($hacks as $hack): ?>
+    <div>
+        <div>
+        <h1><?php echo $hack["title"] ;?></h1>
+        </div>
+        <div>
+        <p><?php echo $hack["body"] ;?></p>
+        </div>
+    </div>
+    <?php endforeach;?>
+</html>
