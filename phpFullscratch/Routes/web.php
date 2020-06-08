f16bd74657d547ea4a7d79efd9fb5f67d9f4551f
ElectricBrandy <yoshitaka.kishi@outlook.jp>
2020-06-08 17:14:57 +0900

    ルーティングとコントローラーの実装
    web.phpからPostsControllerで記述した文面を出力させる

--- phpFullscratch/Routes/web.php
+++ phpFullscratch/Routes/web.php
@@ -1,5 +1,21 @@
 <?php
 
 $url_text = $_GET['url'];
+$params = explode("/", $url_text);
 
-echo $url_text;
+// var_dump ($params."<br>");
+// var_dump ($params[0]."<br>");
+// var_dump ($params[1]."<br>");
+
+$webroot = $_SERVER['DOCUMENT_ROOT'];
+include($webroot."/../app/HTTP/Controllers/PostsController.php");
+$postsController = new PostsController();
+
+switch ($params[0]){
+    case"":
+        $postsController->index();
+        break;
+    case "create":
+        $postsController->create();  
+        break;
+}
--- phpFullscratch/app/HTTP/Controllers/PostsController.php
+++ phpFullscratch/app/HTTP/Controllers/PostsController.php
@@ -1,8 +1,20 @@
 <?php
-//CONTROLLER
 
-$postmodel = new Post();
-$result = $postmodel->index();
-$posts = $result;
+class PostsController
+{
+   public function index()
+   {
+       echo "This is Index Page.";
+   }
+   public function create()
+   {
+       echo "This is Create Page.";
+   }
+}
+
+
+// $postmodel = new Post();
+// $result = $postmodel->index();
+// $posts = $result;
 
 ?>
